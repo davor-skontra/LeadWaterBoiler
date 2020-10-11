cb9492a6f7c30fb3d375375e920b77e66ef1a21f
theKrad23 <thekrad23@gmail.com>
2020-10-11 14:30:09 +0300

    Reorder KeyValuePair params

--- Utilities/KeyValuePairUtility.cs
+++ Utilities/KeyValuePairUtility.cs
@@ -4,8 +4,11 @@ namespace AlkarInjector.Utilities
 {
     public static class KeyValuePairUtility
     {
-        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> self, out TKey key,
-            out TValue value)
+        public static void Deconstruct<TKey, TValue>(
+            this KeyValuePair<TKey, TValue> self,
+            out TKey key,
+            out TValue value
+        )
         {
             key = self.Key;
             value = self.Value;
