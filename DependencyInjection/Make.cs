b7b0ef92581d66b910f83e7b1c34b3a21fb50c84
theKrad23 <thekrad23@gmail.com>
2020-11-15 19:29:13 +0200

    Add instantiator with di.

---
+++ DependencyInjection/Make.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace DependencyInjection
+{
+    public static class Make
+    {
+        public static TMonoBehaviour Injected<TMonoBehaviour>(TMonoBehaviour behaviour, Transform parent = null)
+            where TMonoBehaviour : MonoBehaviour
+        {
+            var result = Object.Instantiate(behaviour, parent);
+            foreach (var injectable in result.GetComponentsInChildren<MonoBehaviour>())
+            {
+                IocContainer.Inject(injectable);
+            }
+
+            return result;
+        }
+        
+        public static GameObject Injected(GameObject original, Transform parent = null)
+        {
+            var result = Object.Instantiate(original, parent);
+            
+            foreach (var injectable in result.GetComponentsInChildren<MonoBehaviour>())
+            {
+                IocContainer.Inject(injectable);
+            }
+
+            return result;
+        }
+    }
+}
