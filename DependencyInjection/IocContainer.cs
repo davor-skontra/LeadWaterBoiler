38d40dceafe4a1dc0895ef3cc87cfe6cfaaa64e5
theKrad23 <thekrad23@gmail.com>
2021-01-09 22:13:48 +0200

    Move the service container to a separate file

--- DependencyInjection/IocContainer.cs
+++ DependencyInjection/IocContainer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace DependencyInjection
 {
-    public static class IocContainer
+    public static partial class IocContainer
     {
         private static readonly Dictionary<Type, KnownType> _knownTypes = new Dictionary<Type, KnownType>();
         public static ServiceContainer Services { get; } = new ServiceContainer();
@@ -37,72 +37,6 @@ namespace DependencyInjection
             }
 
             _knownTypes[type].Resolve(self);
-        }
-
-        public class ServiceContainer
-        {
-            private readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
-
-            public void Clear() => _services.Clear();
-
-            public void Unregister<TService>()
-            {
-                var type = typeof(TService);
-                
-                Unregister(type);
-            }
-
-            public void Unregister(Type type)
-            {
-                if (!_services.ContainsKey(type))
-                {
-                    throw ServiceLocatorException.ShouldExist(type);
-                }
-
-                _services.Remove(type);
-            }
-
-            public void Register<TService>(TService service)
-            {
-                var type = typeof(TService);
-
-                if (_services.ContainsKey(type))
-                {
-                    throw ServiceLocatorException.ShouldNotExist(type);
-                }
-
-                _services[type] = service;
-            }
-
-            public void Register<TRegisterAs, TService>(TService service) where TService : TRegisterAs
-            {
-                Register(service);
-            }
-
-            public object ResolveAnonymous(Type type)
-            {
-                if (_services.ContainsKey(type))
-                {
-                    return _services[type];
-                }
-
-                throw ServiceLocatorException.ShouldExist(type);
-            }
-
-            public TType Resolve<TType>() => (TType) ResolveAnonymous(typeof(TType));
-
-            public class ServiceLocatorException : Exception
-            {
-                private ServiceLocatorException(string message) : base(message)
-                {
-                }
-
-                public static ServiceLocatorException ShouldNotExist(Type type) =>
-                    new ServiceLocatorException($"Service Locator already contains type {type})");
-
-                public static ServiceLocatorException ShouldExist(Type type) =>
-                    new ServiceLocatorException($"Service Locator does not contains type {type})");
-            }
         }
     }
 }
